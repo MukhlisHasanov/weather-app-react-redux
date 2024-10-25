1df7edcef73bf14c5dcc18abf055b5b23546c84f
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-10-25 21:25:51 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import GlobalStyles from "styles/GlobalStyles"
-import { Route, Routes } from "react-router-dom"
 
-import EmployeeLayout from "pages/EmployeeApp/components/EmployeeLayout/EmployeeLayout"
-import EmployeeForm from "pages/EmployeeApp/components/EmployeeForm/EmployeeForm"
-import EmployeeCard from "pages/EmployeeApp/components/EmployeeCard/EmployeeCard"
 
-import { APP_EMPLOYEE_ROUTES } from "constants/routes"
+import { APP_ROUTES } from "constants/routes"
 
-// HOMEWORKS
-import Homework_15 from "homeworks/Homework_15/Homework_15"
-import Homework_16 from "homeworks/Homework_16/Homework_16"
-import Homework_17 from "homeworks/Homework_17/Homework_17"
-// LESSONS
-import Lesson_16 from "lessons/Lesson_16/Lesson_16"
-import Lesson_17 from "lessons/Lesson_17/Lesson_17"
+
 
 
 //CONSULTATIONS
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,17 +1,7 @@
-export enum APP_ROUTES {
-  HOME = "/",
-  ABOUT = "/about",
-  CONTACT_US = "/contactUs",
-  LOGIN = "/login",
-  CLIENTS = "/clients",
-  GOOGLE = "/clients/google",
-  APPLE = "/clients/apple",
-  FACEBOOK = "/clients/facebook",
-  NOT_FOUND = "*",
-}
 
-export enum APP_EMPLOYEE_ROUTES {
-  CREATE_EMPLOYEE = "/",
-  EMPLOYEES = "/employees",
+
+export enum APP_ROUTES {
+  HOME = "/home",
+  WEATHERS = "/weathers",
   NOT_FOUND = "*",
 }
