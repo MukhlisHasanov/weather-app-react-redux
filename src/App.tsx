e0b58ed32d3c2ee30a6765f60c1c33360bc9c6a3
AGorbovyi <anton.gorbovyi@gmail.com>
2024-10-25 21:19:21 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,32 @@
 import GlobalStyles from "styles/GlobalStyles"
 import { Route, Routes } from "react-router-dom"
 
-import EmployeeLayout from "pages/EmployeeApp/components/EmployeeLayout/EmployeeLayout"
-import EmployeeForm from "pages/EmployeeApp/components/EmployeeForm/EmployeeForm"
-import EmployeeCard from "pages/EmployeeApp/components/EmployeeCard/EmployeeCard"
+import Layout from "pages/Layout/Layout"
+import Home from "pages/Home/Home"
+import Weathers from "pages/Weathers/Weathers"
 
-import { APP_EMPLOYEE_ROUTES } from "constants/routes"
-
-// HOMEWORKS
-import Homework_15 from "homeworks/Homework_15/Homework_15"
-import Homework_16 from "homeworks/Homework_16/Homework_16"
-import Homework_17 from "homeworks/Homework_17/Homework_17"
-// LESSONS
-import Lesson_16 from "lessons/Lesson_16/Lesson_16"
-import Lesson_17 from "lessons/Lesson_17/Lesson_17"
-
-
-//CONSULTATIONS
+import { APP_ROUTES } from "constants/routes"
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      {/* HOMEWORKS */}
-      {/* <Homework_15 /> */}
-      {/* <Homework_16 /> */}
-      {/* <Homework_17 /> */}
-      {/* // LESSONS */}
-      {/* <Lesson_16 /> */}
-      {/* <Lesson_17 /> */}
-      {/* //CONSULTATIONS */}
-      <EmployeeLayout>
+      <Layout>
         <Routes>
           <Route
-            path={APP_EMPLOYEE_ROUTES.CREATE_EMPLOYEE}
-            element={<EmployeeForm />}
+            path={APP_ROUTES.HOME}
+            element={<Home />}
           />
           <Route
-            path={APP_EMPLOYEE_ROUTES.EMPLOYEES}
-            element={<EmployeeCard />}
+            path={APP_ROUTES.WEATHERS}
+            element={<Weathers />}
           />
           <Route
-            path={APP_EMPLOYEE_ROUTES.NOT_FOUND}
+            path={APP_ROUTES.NOT_FOUND}
             element="Page Is Not Found"
           />
         </Routes>
-      </EmployeeLayout>
+      </Layout>
     </>
   )
 }
