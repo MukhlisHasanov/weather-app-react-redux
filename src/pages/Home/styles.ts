35a497a3741ede98828753cf4cc7ea11dcd15c5f
AGorbovyi <anton.gorbovyi@gmail.com>
2024-10-26 16:20:22 +0200

    styles for search form. Button styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,61 +1,46 @@
 import styled from "@emotion/styled"
 
 export const PageWrapper = styled.div`
-display: flex;
-flex-direction: column;
-`;
+  display: flex;
+  flex-direction: column;
+`
 
 export const SearchForm = styled.div`
   display: flex;
   flex-direction: row;
-  height: 100%;
-  border: 1px solid black;
-  text-align: center;
+  height: 48px;
   gap: 30px;
-  padding: 60px;
-  width: 600px;
+  padding: 0px;
+  width: 710px;
   border-radius: 4px;
   background-color: #ffffff;
-`;
+`
 
 export const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 470px;
+  height: 48px;
+  width: 550px;
   gap: 20px;
   font-size: 44px;
   text-align: left;
-`;
-
+  border: 1px solid black;
+`
 
 export const SearchButtonContainer = styled.div`
+  height: 48px;
+`
 
-`;
+export const WeatherBar = styled.div``
 
-export const WeatherBar = styled.div`
+export const WeatherContainer = styled.div``
 
-`;
+export const WeatherCondition = styled.div``
 
-export const WeatherContainer = styled.div`
+export const Temperature = styled.div``
 
-`;
+export const City = styled.div``
 
-export const WeatherCondition = styled.div`
+export const Icons = styled.div``
 
-`;
-
-export const Temperature = styled.div`
-
-`;
-
-export const City = styled.div`
-
-`;
-
-export const Icons = styled.div`
-
-`;
-
-export const ButtonContainer = styled.div`
-
-`;
+export const ButtonContainer = styled.div``
